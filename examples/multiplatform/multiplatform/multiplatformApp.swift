eb12cbd86b43330d0b413d079a307f6a271186bd
David House <davidahouse@gmail.com>
2022-08-27 14:25:23 -0400

    :tada: Initial commit

---
+++ examples/multiplatform/multiplatform/multiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  multiplatformApp.swift
+//  multiplatform
+//
+//  Created by David House on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct multiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ xcresultkittest/xcresultkittest/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  xcresultkittest
+//
+//  Created by David House on 8/27/22.
+//
+
+import Foundation
+
+print("Hello, World!")
+
